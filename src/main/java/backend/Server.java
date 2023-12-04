3c5dbb008a9040592ace2ed89474813a5b587009
valentimPaulo02 <valentimspaulo@gmail.com>
2023-12-04 14:06:45 +0000

    fixed client and server error

--- src/main/java/backend/Server.java
+++ src/main/java/backend/Server.java
@@ -1,17 +1,20 @@
 package backend;
 import java.net.*;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Server {
   public static void main(String args[]) {
     try {
+    	
+      Registry registry = LocateRegistry.createRegistry(1099);
     	
       System.out.println("started");
       ServerImplementation addServerImpl = new ServerImplementation();
       Naming.rebind("AddServer", addServerImpl);
       System.out.println("working");
 
-      
     }
     catch(Exception e) {
       System.out.println("Exception: " + e);
--- src/main/java/frontend/ClientRMI.java
+++ src/main/java/frontend/ClientRMI.java
@@ -1,12 +1,17 @@
 package frontend;
 import backend.*;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ClientRMI {
   public static void main(String args[]) {
     try {
+    	
+      Registry registry = LocateRegistry.getRegistry();
+    	
       String addServerURL = "rmi://" + "localhost" + "/AddServer";
-      ServerInterface addServerIntf = (ServerInterface)Naming.lookup(addServerURL);
+      ServerInterface addServerIntf = (ServerInterface)registry.lookup(addServerURL);
       System.out.println("The first number is: " + 1);
       double d1 = 1;
       System.out.println("The second number is: " + 2);
