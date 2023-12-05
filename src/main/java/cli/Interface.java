bf8ffe03a41f5f584bfe14f71e5a35968b39494c
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2023-12-05 12:20:01 +0000

    add bookUmbrella() logic

--- src/main/java/cli/Interface.java
+++ src/main/java/cli/Interface.java
@@ -113,11 +113,131 @@ public class Interface {
             System.out.println("User registered successfully");
         }
     }
+    
+    /////////////////////////////////////////////////////////////////////////////////////
 
     private static void bookUmbrella() {
-        //umbrella reservation logic - do later
-        System.out.println("Booking an unbrella");
+        System.out.println("Choose the beach (A, B or C):");
+        String beach = scanner.nextLine();
+        System.out.println("Choose the number of people (2, 3 or 4):");
+        int numberOfPeople = scanner.nextInt();
+        scanner.nextLine(); //clean buffer
+
+        //check the chosen beach and availability of umbrellas
+        switch (beach.toUpperCase()) {
+            case "A":
+                if (numberOfPeople == 2) {
+                    bookUmbrellaForTwoAtBeachA();
+                } else if (numberOfPeople == 3) {
+                    bookUmbrellaForThreeAtBeachA();
+                } else if (numberOfPeople == 4) {
+                    bookUmbrellaForFourAtBeachA();
+                } else {
+                    System.out.println("Invalid number of people for beach A!");
+                }
+                break;
+            case "B":
+                if (numberOfPeople == 2) {
+                    bookUmbrellaForTwoAtBeachB();
+                } else if (numberOfPeople == 3) {
+                    bookUmbrellaForThreeAtBeachB();
+                } else if (numberOfPeople == 4) {
+                    bookUmbrellaForFourAtBeachB();
+                } else {
+                    System.out.println("Invalid number of people for beach B!");
+                }
+                break;
+            case "C":
+                if (numberOfPeople == 2) {
+                    bookUmbrellaForTwoAtBeachC();
+                } else {
+                    System.out.println("Invalid number of people for beach C!");
+                }
+                break;
+            default:
+                System.out.println("Invalid beach!");
+        }
     }
+
+    private static void bookUmbrellaForTwoAtBeachA() {
+        int availableUmbrellasForTwoAtBeachA = 10; 
+        if (availableUmbrellasForTwoAtBeachA > 0) {
+            availableUmbrellasForTwoAtBeachA--;
+            System.out.println("Booking umbrella in A for 2 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+            System.out.println("There are no umbrellas available for 2 people on beach A.");
+        }
+    }
+
+    // Funções similares para os outros casos na praia A (3 e 4 pessoas)
+    private static void bookUmbrellaForThreeAtBeachA() {
+        int availableUmbrellasForThreeAtBeachA = 5;
+        if (availableUmbrellasForThreeAtBeachA > 0) {
+        	availableUmbrellasForThreeAtBeachA--;
+            System.out.println("Booking umbrella in A for 3 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 3 people on beach A.");
+        }
+    }
+
+    private static void bookUmbrellaForFourAtBeachA() {
+        int availableUmbrellasForFourAtBeachA = 4;
+        if (availableUmbrellasForFourAtBeachA > 0) {
+        	availableUmbrellasForFourAtBeachA--;
+            System.out.println("Booking umbrella in A for 4 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 4 people on beach A.");
+        }
+    }
+    private static void bookUmbrellaForTwoAtBeachB() {
+        int availableUmbrellasForTwoAtBeachB = 5;
+        if (availableUmbrellasForTwoAtBeachB > 0) {
+        	availableUmbrellasForTwoAtBeachB--;
+            System.out.println("Booking an umbrella in B for 2 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 2 people on beach B.");
+        }
+    }
+
+    private static void bookUmbrellaForThreeAtBeachB() {
+        int availableUmbrellasForThreeAtBeachB = 4;
+        if (availableUmbrellasForThreeAtBeachB > 0) {
+        	availableUmbrellasForThreeAtBeachB--;
+            System.out.println("Booking an umbrella in B for 3 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 3 people on beach B.");
+        }
+    }
+
+    private static void bookUmbrellaForFourAtBeachB() {
+        int availableUmbrellasForFourAtBeachB = 1;
+        if (availableUmbrellasForFourAtBeachB > 0) {
+        	availableUmbrellasForFourAtBeachB--;
+            System.out.println("Booking an umbrella in B for 4 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 4 people on beach B.");
+        }
+    }
+
+    private static void bookUmbrellaForTwoAtBeachC() {
+        int availableUmbrellasForTwoAtBeachC = 10;
+        if (availableUmbrellasForTwoAtBeachC > 0) {
+        	availableUmbrellasForTwoAtBeachC--;
+            System.out.println("Booking an umbrella in C for 2 people...");
+            System.out.println("Reservation made successfully!");
+        } else {
+        	System.out.println("There are no umbrellas available for 2 people on beach C.");
+        }
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////
+
 
     private static void cancelReservedUmbrella() {
         //umbrella reservation cancellation logic
