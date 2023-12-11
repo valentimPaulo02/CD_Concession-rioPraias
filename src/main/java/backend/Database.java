88e30584e982dafeff6a71fd09a397fc4779a407
valentimPaulo02 <valentimspaulo@gmail.com>
2023-12-11 15:56:22 +0000

    dbconnection

--- src/main/java/backend/Database.java
+++ src/main/java/backend/Database.java
@@ -1,5 +1,43 @@
 package backend;
+import java.sql.*;
+import java.util.Map;
 
 public class Database {
+	
+	private Connection connection;
+    private String jdbcUrl = "jdbc:mysql://aws.connect.psdb.cloud/cd_project?sslMode=VERIFY_IDENTITY";
+    private String username = "8fcpk2l571ttfuetcc7z";
+    private String password = "pscale_pw_oJpbrXezCuqRs6iPVTNSN7HX3eJzFA5tupFkdOjBE5m";
+    
+    public Database() throws SQLException {
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        connection = DriverManager.getConnection(jdbcUrl, username, password);
+    }
+    
+    public void disconnect() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
 
+    public ResultSet executeQueryWithData(String query, Map<Integer, Object> parameters) throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            throw new SQLException("Database connection is not established.");
+        }
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            // Set parameters for the prepared statement
+            for (Map.Entry<Integer, Object> entry : parameters.entrySet()) {
+                preparedStatement.setObject(entry.getKey(), entry.getValue());
+            }
+
+            // Execute the query
+            return preparedStatement.executeQuery();
+        }
+    }
+	
 }
--- src/main/java/backend/ServerImplementation.java
+++ src/main/java/backend/ServerImplementation.java
@@ -1,12 +1,39 @@
 package backend;
 import java.rmi.*;
 import java.rmi.server.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
 
   public ServerImplementation() throws RemoteException {
   }
+  
   public double add(double d1, double d2) throws RemoteException {
-    return d1 + d2;
+	  
+	  try {
+		  Database db = new Database();
+		  
+		  String query = "INSERT INTO Booking (booking_id, user_id, beach_id, service_id, date, start_time, ending_time)"
+		  		+ " VALUES (?, ?, ?, ?, ?, ?, ?)";
+		  Map<Integer, Object> parameters = new HashMap<>();
+		  parameters.put(1, "2");
+		  parameters.put(2, "1");
+		  parameters.put(3, "A");
+		  parameters.put(4, "A11");
+		  parameters.put(5, "2023-06-17");
+		  parameters.put(6, "8");
+		  parameters.put(7, "9");
+		  
+		  ResultSet result = db.executeQueryWithData(query, parameters);
+		  db.disconnect();
+		  
+	  }catch (SQLException e) {
+		  e.printStackTrace();
+	  }
+	   
+	  return d1 + d2;
   }
 }
