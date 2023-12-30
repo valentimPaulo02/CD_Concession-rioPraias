994545b66b77736e861b9a6e8e177436a9cc4c92
valentimPaulo02 <100430380+valentimPaulo02@users.noreply.github.com>
2023-12-30 10:57:06 +0000

    Update Booking.java

--- src/main/java/objects/Booking.java
+++ src/main/java/objects/Booking.java
@@ -19,8 +19,7 @@ public class Booking implements Serializable{
 	private int EndingTime;
 	
 	
-	public Booking(int Id, int UserId, String ServiceId, String BeachId, String Date, int StartTime, int EndingTime) {
-		this.Id = Id;
+	public Booking(int UserId, String ServiceId, String BeachId, String Date, int StartTime, int EndingTime) {
 		this.UserId = UserId;
 		this.ServiceId = ServiceId;
 		this.BeachId = BeachId;
@@ -28,8 +27,8 @@ public class Booking implements Serializable{
 		this.StartTime = StartTime;
 		this.EndingTime = EndingTime;
 	}
-	public Booking(int UserId, String ServiceId, String Date, int StartTime, int EndingTime) {
-		this.UserId = UserId;
+	public Booking(int Id, String ServiceId, String Date, int StartTime, int EndingTime) {
+		this.Id = Id;
 		this.ServiceId = ServiceId;
 		this.Date = Date;
 		this.StartTime = StartTime;
